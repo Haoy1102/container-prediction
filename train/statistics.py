95125fbea60677aee5ab96a66abb60d0b2c4c3a2
haoy <hyhaoyang1@163.com>
2023-04-04 20:44:34 +0800

    bug改——hit_rates以及代码逻辑改通，可运行

--- train/statistics.py
+++ train/statistics.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 data = []
 # 加载数据集并解析为JSON格式。
 json_files = []
-for root, dirs, files in os.walk("../dataset/test"):
+for root, dirs, files in os.walk("../dataset/node-9e-7.13"):
     for file in files:
         if file.endswith(".json"):
             json_file = os.path.join(root, file)
@@ -19,12 +19,12 @@ for json_file in tqdm(json_files):
 
 uris = []
 for item in data:
-    uri_parts = item["http.request.uri"].split("/")[2:3] or ["default"]
+    uri_parts = item["http.request.uri"].split("/")[1:3] or ["default"]
     uris.append("/".join(uri_parts))
 
 uri_counts = Counter(uris)
 total_count = sum(uri_counts.values())
-print(f"共有{total_count}种不同的v2后面两个部分，记录如下：")
+print(f"共有{total_count}条数据")
 print(f"共有{len(uri_counts)}种不同的v2后面两个部分，记录如下：")
 for uri, count in uri_counts.most_common():
     print(f"{uri}: {count} 次，占比{count/total_count:.2%}")
--- worked/drl.py
+++ worked/drl.py
@@ -1,103 +1,118 @@
-import gym
 import torch.nn.functional as F
-import random
-import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import json
+import os
+from tqdm import tqdm
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import re
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, LabelEncoder
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+import random
+import gym
+import numpy as np
+
 
 class EdgeNodeEnv(gym.Env):
-    def __init__(self, data):
-        self.data = data
-        self.memory_size = 10  # 内存大小设为10
+    def __init__(self, data, memory_size):
+        self.data = data  # 数据集合
+        self.memory_size = memory_size  # 内存大小
         self.containers_in_memory = set()  # 当前在内存中的容器集合
-        self.action_space = gym.spaces.Discrete(2)  # 动作空间有两种：0表示载入新容器，1表示清除旧容器
-        self.observation_space = gym.spaces.Box(low=0, high=1, shape=(len(data[0]) - 2,),
-                                                dtype=float)  # 观测空间由数据项中除了host和timestamp以外所有属性组成
+
+        # 定义动作空间和观察空间。
+        self.action_space = gym.spaces.Discrete(2)
+        self.observation_space = gym.spaces.Box(low=-1, high=1000, shape=(memory_size + 2,), dtype=float)
 
     def reset(self):
         """
-        重置环境状态并返回初始观测值。
-        在本例中，我们假定最初没有任何容器被加载到内存中。
-        Returns:
-            observation (object): 初始观察值。
+        环境重置函数，在每个 episode 开始时调用。
+        返回值：
+            observation: ndarray. 初始状态下环境观测到的状态信息。
         """
+        # 清除当前内存中所有容器。
         self.containers_in_memory.clear()
+        # 初始化 current_step 变量为第一个请求位置.
+        self.current_step = 0
         return self._get_observation()
 
     def step(self, action):
-        """
-        采取给定动作并返回更新后的状态、奖励、是否终止等信息。
-        Args:
-           action (object): 要执行的动作。
-        Returns:
-           observation (object): 更新后的观察值。
-           reward (float) : 由于执行动作而获得的奖励。
-           done (bool): 是否到达终止状态。在本例中，我们假设永远不会结束。
-        """
-        if action == 0:  # 载入新容器
-            container_type = self._get_container_type()
-            if len(self.containers_in_memory) < self.memory_size and container_type not in self.containers_in_memory:
-                # 如果内存未满且该类型容器不在内存中，则将其载入内存并给予正向奖励
-                self.containers_in_memory.add(container_type)
-                reward = 1.0
-            else:
-                # 否则给予负向奖励，并保持现有状态不变
-                reward = -1.0
-        elif action == 1:  # 清除旧容器
-            if len(self.containers_in_memory) > 0:
-                containers_by_age = sorted(list(self.containers_in_memory), key=lambda x: "/".join(x.split("/")[1:3]))
-
-                oldest_container = containers_by_age[0]
-
-                if oldest_container in self.containers_in_memory:
-                    # 将最老的容器清出内存并给予正向奖励
-                    self.containers_in_memory.remove(oldest_container)
-                    reward = 1.0
-
-            else:
-                # 如果没有可清除的容器，则保持现有状态不变，并给予负向奖励
-                reward = -1.0
+        hit = False
+        done = False
+        # 获取当前请求容器类型
+        container_type = self.data[self.current_step]
+        if container_type in self.containers_in_memory:
+            reward = 3.0  # 命中缓存，给予正向奖励
+            hit = True
         else:
-            raise ValueError("Invalid action: {}".format(action))
+            reward = -1.0  # 没有命中缓存，给予负向奖励
+            if len(self.containers_in_memory) >= self.memory_size:
+                # 内存已满, 需要清除一个容器再加载新容器
+                containers_to_remove = []
+                for c in self.containers_in_memory:
+                    if np.random.uniform() < (1 / len(self.containers_in_memory)):
+                        containers_to_remove.append(c)
 
-        print("Memory:", self.containers_in_memory)
-        return self._get_observation(), reward, False, {}
+                if not containers_to_remove:  # 如果没有找到要删除的容器，则随机选择一个进行删除
+                    container_to_remove = np.random.choice(list(self.containers_in_memory))
+                    self.containers_in_memory.remove(container_to_remove)
 
-    def _get_container_type(self):
-        """
-        从数据集中随机选择一个容器类型。
-        Returns:
-            container_type (str): 容器类型。
-        """
-        var = []
-        for d in self.data:
-            if "http.request.uri" in d and len(d["http.request.uri"].split("/")) >= 4:
-                uri_parts = d["http.request.uri"].split("/")
-                var.append(uri_parts[1] + "/" + uri_parts[2])
+                else:  # 找到了要删除的容器，则从列表中移除即可
+                    for c in containers_to_remove:
+                        self.containers_in_memory.remove(c)
+
+                self.containers_in_memory.add(container_type)
+
             else:
-                var.append("default_value")
-        return random.choice(var)
+                # 内存未满, 直接将新容器加入内存
+                self.containers_in_memory.add(container_type)
+
+            if action == 0:  # 不保留缓存
+                if container_type in self.containers_in_memory:
+                    self.containers_in_memory.remove(container_type)
+
+            elif action == 1:  # 保留缓存
+                pass
+
+        if (self.current_step + 1) == len(self.data):
+            done = True
+
+        info = {'hit': hit}
+        observation = self._get_observation()
+        self.current_step += 1  # 更新当前处理的请求位置.
+
+        return observation, reward, done, info
 
     def _get_observation(self):
         """
-        返回当前状态的观察值。
-        Returns:
-           observation (object): 当前状态的观察值。
+        返回当前环境观测到的状态信息.
+        当前请求的容器类型是否在内存中。如果是，则表示已经命中缓存；如果不是，则需要加载到内存。
+        内存利用率。可以通过计算当前内存使用情况与总内存大小之比来得出。
+        缓存在内存中的其他容器信息。例如，在清除一个缓存在内存中的容器时，可能希望选择最少被访问过或者大小较小等策略进行清理。
+        Returns :
+            ndarray : 状态信息数组.
         """
-        obs = []
+        # 获取当前请求容器类型
+        container_type = self.data[self.current_step]
 
-        for key in self.data[0]:
-            if key != "host" and key != "timestamp":
-                obs.append(1.0 if any(key in d and d[key] is not None for d in self.data) else 0.0)
+        # 计算当前内存利用率
+        memory_utilization = len(self.containers_in_memory) / self.memory_size
 
-        return obs
+        # 创建一个长度为 self.memory_size 的全零数组
+        observation = np.full(shape=(self.memory_size,), fill_value=-1)
+        containers_sorted = sorted(self.containers_in_memory)
+        # 将缓存在内存中的其他容器信息填充到 observation 中
+        for i in range(len(containers_sorted)):
+            observation[i] = containers_sorted[i]
 
+        # 在 observation 数组开头添加当前请求容器类型是否在内存中和当前内存利用率两个元素
+        observation = np.concatenate(([int(container_type in self.containers_in_memory), memory_utilization],
+                                      observation))
 
-
+        return observation
 
 
 class DQN(nn.Module):
@@ -124,6 +139,7 @@ class DQNAgent:
         self.epsilon_start = 1.0  # 贪婪策略起始值
         self.epsilon_end = 0.01  # 贪婪策略结束值
         self.epsilon_decay = 5000  # epsilon线性衰减的步数
+        self.learning_rate = 0.001
 
         # 初始化经验回放缓冲区、当前状态和epsilon值。
 
@@ -134,12 +150,17 @@ class DQNAgent:
         self.current_epsilon = self.epsilon_start
 
         # 将模型和优化器放置在GPU上
+
         self.model = DQN(env.observation_space.shape[0], env.action_space.n).to(device)
-        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
         self.loss_fn = nn.MSELoss()
 
     def act(self, state):
         # 根据当前状态选择动作。
+
+        device = next(self.model.parameters()).device  # 获取设备信息
+
+        # 随机选择动作
         if random.random() < self.current_epsilon:
             return self.env.action_space.sample()
         else:
@@ -148,18 +169,9 @@ class DQNAgent:
                 return q_values.argmax().item()
 
     def replay_memory(self, batch_size):
-        #TODO 数据类型不对
         # 从经验回放缓冲区中随机抽取一批样本，并使用它们更新模型参数。
-        for exp in self.memory:
-            state, action, reward, next_state = exp
-            print("Experience:", exp)
-            print("Types:", type(state), type(action), type(reward), type(next_state))
 
-            if not all(isinstance(i, (float, int)) for i in [state, next_state]) or not isinstance(action,
-                                                                                                   int) or not isinstance(
-                    reward, float):
-                print("Invalid data types in experience replay buffer!")
-                # 在此处添加适当修改来修复不正确的数据类型。
+        device = next(self.model.parameters()).device  # 获取设备信息
 
         if len(self.memory) < batch_size:
             return
@@ -168,6 +180,8 @@ class DQNAgent:
 
         states, actions, rewards, next_states = zip(*transitions)
 
+        states = np.array(states)
+        next_states = np.array(next_states)
         states = torch.tensor(states).float().to(device)
         actions = torch.tensor(actions).unsqueeze(1).to(device)
         rewards = torch.tensor(rewards).unsqueeze(1).to(device)
@@ -188,7 +202,7 @@ class DQNAgent:
         state = self.current_state
         action = self.act(state)
 
-        next_state, reward, done, _ = self.env.step(action)
+        next_state, reward, done, info = self.env.step(action)
         # 将经验存储到经验回放缓冲区中。
         self.memory.append((state, action, reward, next_state))
 
@@ -199,49 +213,113 @@ class DQNAgent:
         self.current_state = next_state
         self.current_epsilon = max(self.epsilon_end,
                                    self.current_epsilon - (self.epsilon_start - self.epsilon_end) / self.epsilon_decay)
-        return reward
 
-    def train(self, num_steps):
+        return reward, done, info
+
+    def train(self, num_epochs, len_data):
         total_reward = 0.0
-        for i in range(num_steps):
-            reward = self.train_step()
+        total_hit = 0.0
+        self.current_state = env.reset()
+        for i in range(num_epochs):
+            reward, done, info = self.train_step()
             total_reward += reward
-
-        return total_reward
-
-
-import matplotlib.pyplot as plt
-import json
-import os
-from tqdm import tqdm
+            total_hit += 1 if info.get('hit') else 0
+            if done:
+                break
+            # print("num_epochs:{}".format(i))
+        hit_rate = total_hit / len_data
+        return total_reward, hit_rate
 
 
 # 加载数据集并解析为JSON格式。
+# json_files = []
+# for root, dirs, files in os.walk("../dataset/test"):
+#     for file in files:
+#         if file.endswith(".json"):
+#             json_file = os.path.join(root, file)
+#             json_files.append(json_file)
+#
+# data = []
+# for json_file in tqdm(json_files):
+#     with open(json_file, "r") as f:
+#         json_object = json.load(f)
+#         data.extend(json_object)
+
+
+# ----------------------------------------------------------------
+def transform_data(request):
+    # 提取特征
+    uri = request["http.request.uri"]
+
+    match = re.search(r"v2/([^/]+)/([^/]+)", uri)
+    if match:
+        uri_parts = match.groups()
+        uri = "/".join(uri_parts)
+    else:
+        uri = "v2"
+
+    return [uri]
+
+
+def preprocess_data(raw_data):
+    # 将原始数据转换为数据帧
+    df = pd.DataFrame(raw_data, columns=["container_type"])
+
+    # 使用标签编码
+    container_type = df["container_type"].values
+    label_encoder = LabelEncoder()
+    container_type = label_encoder.fit_transform(container_type)
+
+    return container_type
+
+
+# 加载数据集
 json_files = []
-for root, dirs, files in os.walk("../dataset/node-9e"):
+for root, dirs, files in os.walk("../dataset/node-9e-7.13"):
     for file in files:
         if file.endswith(".json"):
             json_file = os.path.join(root, file)
             json_files.append(json_file)
 
-data = []
-for json_file in tqdm(json_files):
-    with open(json_file, "r") as f:
-        json_object = json.load(f)
-        data.extend(json_object)
+
+def load_data_generator(json_files):
+    for json_file in tqdm(json_files, desc="Loading JSON files"):
+        with open(json_file, 'r') as f:
+            try:
+                requests = json.load(f)
+                for request in requests:
+                    data = transform_data(request)
+                    yield data
+            except json.JSONDecodeError as json_err:
+                print("JSONDecodeError: " + str(json_err) + " in JSON file: " + json_file)
+            except ValueError as value_err:
+                print("ValueError: " + str(value_err) + " in JSON file: " + json_file)
 
 
-env = EdgeNodeEnv(data)
-agent = DQNAgent(env)
+raw_data = load_data_generator(json_files)
+data = preprocess_data(raw_data)
+# ----------------
+memory_size = 10
+len_data = len(data)
 rewards = []
+hit_rates = []
+
+env = EdgeNodeEnv(data, memory_size)
+agent = DQNAgent(env)
 for i in range(100):
-    agent.current_state = env._get_container_type()
-    reward = agent.train(num_steps=100)
+    result = agent.train(num_epochs=5000, len_data=len_data)
+    reward, hit_rate = result
     rewards.append(reward)
+    hit_rates.append(hit_rate)
     print("epoch:{}".format(i))
 
 # plt.switch_backend('TkAgg')
 plt.plot(range(len(rewards)), rewards)
 plt.xlabel("Episode")
 plt.ylabel("Total Reward")
+plt.show()
+
+plt.plot(range(len(hit_rates)), hit_rates)
+plt.xlabel("Episode")
+plt.ylabel("Hit rates")
 plt.show()
